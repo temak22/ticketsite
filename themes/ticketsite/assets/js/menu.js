2089f7951ae16bc058cfdc2ddfa531c408eb75d7
TuchinaN <tuchina.natalia2002@mail.ru>
2022-04-20 17:45:59 +0300

    Added order creation

--- themes/ticketsite/assets/js/menu.js
+++
@@ -1,7 +0,0 @@
-console.log("Hi");
-$(document).ready(function () {
-  $('.header__burger').click(function (event) {
-    $('.header__burger, nav,header').toggleClass('active');
-    $('body').toggleClass('lock');
-  })
-});
